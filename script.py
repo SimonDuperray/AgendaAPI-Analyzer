23e5a403ea5d8df86e93eeb0d184e01fbe4fdd5f
duperrsi <simon.duperray@reseau.eseo.fr>
2022-03-17 08:59:07 +0100

    :sparkles: store figures locally

--- script.py
+++ script.py
@@ -13,14 +13,16 @@ def store_agenda_in_file(url):
 
 # store_agenda_in_file(url)
 
-def pie(labels, sizes, title):
+def pie(labels, sizes, title, filename):
    colors = ['#800000', '#9A6324', '#808000', '#469990', '#000075', '#e6194B', '#f58231', '#ffe119', '#bfef45', '#3cb44b', '#42d4f4', '#4363d8', '#911eb4', '#f032e6', '#a9a9a9', '#fabed4', '#ffd8b1', '#fffac8', '#aaffc3', '#dcbeff']
    patches, texts, _ = plt.pie(sizes, colors=colors, startangle=90, autopct='%1.1f%%', textprops=dict(color="w"))
    plt.legend(patches, labels, loc="best")
    plt.axis('equal')
    plt.tight_layout()
    plt.title(title)
-   plt.show()
+   # plt.show()
+   plt.savefig('./figures/' + filename + '.png')
+   plt.clf()
 
 # fetch global object
 with open('./data/agendas.json', 'r') as f:
@@ -35,23 +37,23 @@ steps2 = {
    "third floor": steps_repartition['3'],
    "fourth floor": steps_repartition['4'],
 }
-pie(steps2.keys(), steps2.values(), "Floors repartition")
+pie(steps2.keys(), steps2.values(), "Floors repartition", 'floors_repartition')
 
 # steps nb_hour_per_lesson
 nb_hour_per_lesson = agenda['nb_hour_per_lesson']
-pie(nb_hour_per_lesson.keys(), nb_hour_per_lesson.values(), "Hours per lesson")
+pie(nb_hour_per_lesson.keys(), nb_hour_per_lesson.values(), "Hours per lesson", "hours_per_lesson")
 
 # steps teachers_repartition
 teachers_repartition = agenda['teachers_repartition']
-pie(teachers_repartition.keys(), teachers_repartition.values(), "Teachers repartition")
+pie(teachers_repartition.keys(), teachers_repartition.values(), "Teachers repartition", "teachers_repartition")
 
 # steps lessons_types_repartition
 lessons_types_repartition = agenda['lessons_types_repartition']
-pie(lessons_types_repartition.keys(), lessons_types_repartition.values(), "Lessons types repartition")
+pie(lessons_types_repartition.keys(), lessons_types_repartition.values(), "Lessons types repartition", "lessons_types_repartition")
 
 # steps buildings_repartition
 buildings_repartition = agenda['buildings_repartition']
-pie(buildings_repartition.keys(), buildings_repartition.values(), "Buildings repartition")
+pie(buildings_repartition.keys(), buildings_repartition.values(), "Buildings repartition", "buildings_repartition")
 
 # nb total classes and exams
 nb_total_classes = agenda['nb_hours']
